3a567b4bc3d4d7ac0253664fe69db83162ea56c6
Seko1112 <serdar.zhangali@mail.utoronto.ca>
2024-05-16 19:36:56 -0400

    src/FizzBuzz.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,16 +1,20 @@
 /**
  * Solve the FizzBuzz challenge.
  */
+/**
+ * Solve the FizzBuzz challenge.
+ */
 class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        int i = 1;
+
+        while (i < 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
-
             // Print our appropriate result.
             if (divisibleBy3 && divisibleBy5) {
 
@@ -29,6 +33,9 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+
+            i++;
         }
     }
 }
+
